24d820d4af735980cda858df0f2a813669eb34a6
g-theodoroy <g.theodoroy@gmail.com>
2017-01-15 18:48:11 +0200

    Update common.php

--- common.php
+++ common.php
@@ -10,7 +10,7 @@ session_start ();
 date_default_timezone_set ( 'Europe/Athens' );
 
 // Εισαγωγή των κλάσεων του composer
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 
 $style_prefix = '';
