8ed7c436ad4909d6654d623bd0a375e5e2df6544
Γεώργιος Θεοδώρου <g.theodoroy@gmail.com>
2017-04-12 17:31:43 +0300

    Update common.php

--- common.php
+++ common.php
@@ -1,9 +1,8 @@
 <?php
-//error_reporting ( E_ALL & ~ E_STRICT & ~ E_NOTICE );
-
 ini_set('display_startup_errors',1);
 ini_set('display_errors',1);
-error_reporting(-1);
+//error_reporting(-1);
+error_reporting(E_ALL & ~E_NOTICE);
 
 session_start ();
 
